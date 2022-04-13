8ef1d3d54067340a71f40728cc40d297b52442ee
Rickh <hpx210650@gmail.com>
2022-04-13 20:53:21 +0800

    ✨ feat: file-loader

---
+++ README.md
@@ -0,0 +1,2 @@
+## 參考
+[sbayd / react-cv](https://github.com/sbayd/react-cv/blob/master/src/index.js)
--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,11 @@ module.exports = {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ],
       },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader',
+        options: {},
+      },
     ],
   },
   resolve: {
